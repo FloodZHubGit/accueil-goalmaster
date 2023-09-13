6584ae2920e959d33880ebf34094c1b76a0fba55
FloodzHubGit <florentin.dekneudt@epsi.fr>
2023-09-13 23:54:27 +0200

    dinguerie

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,33 @@
+import React, { useState } from "react";
 import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience.jsx";
 import { Loader } from "@react-three/drei";
 import { Suspense } from "react";
+import { LoadingScreen } from "./components/LoadingScreen";
+
+const App = () => {
+  const [start, setStart] = useState(false);
+
+  return (
+    <>
+      <Canvas
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 2000,
+          position: [0, 0, 10],
+        }}
+      >
+        <Suspense fallback={null}>
+          <Experience />
+        </Suspense>
+      </Canvas>
+      <LoadingScreen started={start} onStarted={() => setStart(true)} />
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-root.render(
-  <>
-    <Canvas
-      camera={{
-        fov: 45,
-        near: 0.1,
-        far: 2000,
-        position: [0, 0, 10],
-      }}
-    >
-      <Suspense fallback={null}>
-        <Experience />
-      </Suspense>
-    </Canvas>
-    <Loader />
-  </>
-);
+root.render(<App />);
